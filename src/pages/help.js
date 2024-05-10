93ba1bd36c0ab192e5d7f3d4271ce139ab720c8e
Drew Brantley <drew@drew-mbp.lan>
2024-05-10 10:07:00 -0500

    add help page

---
+++ src/pages/help.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function App() {
+  return (
+    <>
+        <div><a href={'https://github.com/yeltnar/energy_calculations_webui'}>Frontend GitHub</a></div>
+        <div><a href={'https://github.com/yeltnar/energy_calculations'}>Backend GitHub</a></div>
+    </>
+  );
+}
